d84325e624bb2c8deba6c0471de62b80a41d33c2
Miko <kozak.mikolaj@gmail.com>
2023-07-10 13:04:02 +0200

    ssr: false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
+  ssr: false,
   modules: [
       "@pinia/nuxt",
       "@nuxt/devtools"
