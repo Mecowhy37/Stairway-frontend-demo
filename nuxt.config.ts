6bc221d25091d9a4bf46cc598392b0dd2d0f764b
Miko <kozak.mikolaj@gmail.com>
2023-07-11 12:59:06 +0200

    ssr: true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://v3.nuxtjs.org/api/confsiguration/nuxt.config
 export default defineNuxtConfig({
-  // ssr: false,
-  nitro: {
-    prerender: {
-      crawlLinks: true
-    }
-  },
+  ssr: true,
+  // nitro: {
+  //   prerender: {
+  //     crawlLinks: true
+  //   }
+  // },
   modules: [
       "@pinia/nuxt",
       "@nuxt/devtools"
