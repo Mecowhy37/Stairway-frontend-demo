a40d34f12cb4f1bb34cc85d1e6684d48bc8137a5
Miko <kozak.mikolaj@gmail.com>
2023-09-19 17:29:44 +0200

    small tweak

--- app.vue
+++ app.vue
@@ -1,19 +1,7 @@
 <template>
-    <div>
-        <NuxtLayout>
-            <NuxtPage />
-        </NuxtLayout>
-    </div>
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
 </template>
 
-<style lang="scss">
-.web3onboard {
-    position: fixed;
-    bottom: 0;
-    right: 0;
-    top: 0;
-    width: 100%;
-    height: 100dvh;
-    pointer-events: none;
-}
-</style>
+<style lang="scss" scoped></style>
